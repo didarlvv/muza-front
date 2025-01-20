2e698f277db2752f30416e11ed7fa292e6d77f5c
Didar Lalakov <didar@MacBook-Pro-Didar.local>
2025-01-20 19:47:50 +0500

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from "next"
-import { Inter } from 'next/font/google'
-import "./globals.css"
-import { AuthProvider } from '@/contexts/AuthContext'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+// import "../styles/globals.css";
+import { AuthProvider } from "@/contexts/AuthContext";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Muza Restaurant Management",
   description: "Restaurant management system",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
-  )
+  );
 }
-
